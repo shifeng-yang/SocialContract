81e9387ad7ccbc6e952f5eb59e7afb365f98d499
shifeng-yang <1978636353@qq.com>
2020-03-23 09:00:33 +0800

    修改首页

--- module_main/src/main/java/com/ysf/module_main/model/dao/UserAccountDao.java
+++ module_main/src/main/java/com/ysf/module_main/model/dao/UserAccountDao.java
@@ -15,7 +15,6 @@ public class UserAccountDao {
         mHelp = new UserAccountDB(context);
     }
 
-
     public void addAccount(MyUserManage user) {
         mDb = mHelp.getReadableDatabase();
         ContentValues values = new ContentValues();
